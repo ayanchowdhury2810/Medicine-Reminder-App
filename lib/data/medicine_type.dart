49783918d102820f378c5b0855ede0d9438806a9
ayanchowdhury <ayan@evincedev.com>
2025-04-03 00:29:11 +0530

    Pushed

--- lib/data/medicine_type.dart
+++ lib/data/medicine_type.dart
@@ -3,24 +3,20 @@ import '../models/medicine_type_data_model.dart';
 class MedicineType {
   static List<MedicineTypeDataModel> medicineTypesData = [
     MedicineTypeDataModel(
-      imagePath: "assets/icons/bottle.png",
-      desc: "Bottle",
-      isSelected: false
-    ),MedicineTypeDataModel(
-      imagePath: "assets/icons/pills.png",
-      desc: "Pills",
-      isSelected: false
-    ),MedicineTypeDataModel(
-      imagePath: "assets/icons/syringe.png",
-      desc: "Syringe",
-      isSelected: false
-    ),MedicineTypeDataModel(
-      imagePath: "assets/icons/tablet.png",
-      desc: "Tablet",
-      isSelected: false
-    ),
+        imagePath: "assets/icons/bottle.png",
+        desc: "Bottle",
+        isSelected: false),
+    MedicineTypeDataModel(
+        imagePath: "assets/icons/pills.png", desc: "Pills", isSelected: false),
+    MedicineTypeDataModel(
+        imagePath: "assets/icons/syringe.png",
+        desc: "Syringe",
+        isSelected: false),
+    MedicineTypeDataModel(
+        imagePath: "assets/icons/tablet.png",
+        desc: "Tablet",
+        isSelected: false),
   ];
 
   static List<String> hrsList = ['1', '2', '3', '4', '5', '6', '7', '8'];
-
 }
